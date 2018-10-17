1d60caab3278030b634b0d9d08afc273b063cc69
Angela Lu <angela.lu0919@gmail.com>
2018-10-17 00:53:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,7 @@ const codes = [
 
 let index = 0
 
-document.addEventListener('keydown', (e)=>{console.log(e)}, false)
+document.addEventListener('keydown', (e)=>{console.log(e.key)}, false)
 
 function init(e) {
  const key = e.key
