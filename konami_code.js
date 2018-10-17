8332ea64311c9ff4bfd4bd662be6d5490d4aecd3
Angela Lu <angela.lu0919@gmail.com>
2018-10-17 01:11:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,6 @@ let index = 0
 document.addEventListener('keypress', (e)=>{init(e.key)}, false)
 
 function init(e) {
-  console.log(codes.length)
  if(e === codes[index]) {
    index++
    if(index === codes.length) {
